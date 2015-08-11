48ae8672f0cc5a706594673d187ad64cb79bd3d1
Hugh Perkins <hughperkins@gmail.com>
2015-08-11 18:48:47 +0800

    save braces experimentaoitn

--- lua_indents.py
+++ lua_indents.py
@@ -10,6 +10,8 @@ line = sys.stdin.readline()
 indent = 0
 nextindent = 0
 last_line = None
+last_excess_braces = 0
+#brace_indentation = 0
 while line:
   line = line.strip()
   if(line.startswith('if') or line.startswith('for') or line.startswith('while') or line.startswith('function')
@@ -21,7 +23,27 @@ while line:
     indent -= 1
     nextindent -= 1
   # handle brackets...
-  nextindent += line.count('(') - line.count(')')
+  this_excess_braces = line.count('(') - line.count(')')
+  nextindent += this_excess_braces
+  if last_excess_braces == 0 and this_excess_braces > 0:
+    # lets split it out to next line...
+    brace_pos = line.find('(')
+    line_1 = line[:brace_pos + 1]
+    line_2 = line[brace_pos + 1:]
+    sys.stdout.write(' ' * (indentsize * indent) + line_1 + '\n')
+    line = line_2
+    indent += 1
+  last_excess_braces = this_excess_braces
+#  new_excess_braces = excess_braces + this_excess_braces
+#  if excess_braces > 0 or new_excess_braces > 0:
+#    print('brace old=', excess_braces, 'new=', new_excess_braces, 'brace_indentation', brace_indentation)
+##  if excess_braces > 0:
+##    indent += brace_indentation - 1
+#  if excess_braces == 0 and new_excess_braces > 0:
+#    brace_indentation = (line.find('(') + 1 )
+#    print('update brace_indentation to', brace_indentation)
+#  if 
+#  excess_braces = new_excess_braces
   sys.stdout.write(' ' * (indentsize * indent) + line + '\n')
   indent = nextindent
   last_line = line
